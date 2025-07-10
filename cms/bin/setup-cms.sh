bea71d0db0e03e3027463ab0657f3ae8617b0158
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-07-09 19:56:02 -0500

    fix: make setup remote fallback assumes branch
    Change URL to CDN one because it works with branch or tag with same URL.

--- cms/bin/setup-cms.sh
+++ cms/bin/setup-cms.sh
@@ -18,8 +18,7 @@ SRC_ROOT="${SCRIPT_DIR}/../src"
 
 # Configure fallback for settings
 VERSION="main"
-BASE_URL="https://raw.githubusercontent.com/TACC/Core-CMS/refs/tags/${VERSION}"
-# BASE_URL="https://cdn.jsdelivr.net/gh/TACC/Core-CMS@${VERSION}"
+BASE_URL="https://cdn.jsdelivr.net/gh/TACC/Core-CMS@${VERSION}"
 CREATE_VAR_FILES=false
 
 # Functions
