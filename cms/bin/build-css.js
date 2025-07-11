24a3ac45cad71b670ff7a106c13e455d584cee9f
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-07-10 20:18:45 -0500

    refactor: replace build dir with minimal css build

--- cms/bin/build-css.js
+++ cms/bin/build-css.js
@@ -2,21 +2,20 @@
 
 const buildStylesheets = require('@tacc/core-styles').buildStylesheets;
 
-const ROOT = __dirname + '/..';
+const ROOT = __dirname + '/../';
 const BUILD_ID = process.env.BUILD_ID || process.env.npm_package_version;
-const PROJECT_PATH = 'src/taccsite_custom/texascale_cms/static/texascale_cms/css';
+const ASSET_PATH = ROOT + 'src/taccsite_custom/texascale_cms/static/texascale_cms/';
 
 const options = {
   verbose: true,
+  fileExt: '.css',
   buildId: BUILD_ID,
   // If custom configuration is desired, then create and pass this file
   // customConfigs: [`${ROOT}/.postcssrc.extra.yml`],
 };
 
-// TODO: Only build individual files as needed
-// SEE: https://github.com/TACC/Core-CMS-Custom/blob/5c04616/frontera_assets/bin/build-css.js#L16-L23
 buildStylesheets(
-  `${ROOT}/${PROJECT_PATH}/src/**/*.css`,
-  `${ROOT}/${PROJECT_PATH}/build`,
+  `${ASSET_PATH}css/*.postcss`,
+  `${ASSET_PATH}css/`,
   options
 );
